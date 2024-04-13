3c87a08adfe4419bf695fff12e92da5c40ef1fde
israellamarr <israellamarr@gmail.com>
2024-04-13 13:56:12 -0400

    array of restaurants open at specific entered time now returns successfully, cleaned up and simplified some code

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import csv
-from datetime import datetime, timedelta
+from datetime import datetime, time
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -8,13 +8,13 @@ app = Flask(__name__)
 restaurant_data = {}
 
 
-def create_tuples(day_hours, array):
+def create_schedules(day_hours, array):
     parts = day_hours.split(' ')
     days = parts[0] if ',' not in parts[0] else parts[0] + parts[1]
     hours = ' '.join(parts[1:]) if ',' not in parts[0] else ' '.join(parts[2:])
     hour_array = hours.split(' - ')
-    start_hour = hour_array[0]
-    end_hour = hour_array[1]
+    start_datetime = convert_string_time(hour_array[0])
+    end_datetime = convert_string_time(hour_array[1])
     time_frames = {}
 
     if ',' in days:
@@ -22,22 +22,27 @@ def create_tuples(day_hours, array):
         day_array = days.split(',')
         for day in day_array:
             if '-' not in day:
-                time_frames[day] = (start_hour, end_hour)
+                time_frames[day] = (start_datetime, end_datetime)
             else:
                 start_day, end_day = day.split('-')
-                handle_day_list(end_day, end_hour, start_day, start_hour, time_frames)
+                weekly_hours(end_day, end_datetime, start_day, start_datetime, time_frames)
     elif '-' not in days:
         # single day timeframe scenario
-        time_frames[days] = (start_hour, end_hour)
+        time_frames[days] = (start_datetime, end_datetime)
     else:
         # handles Mon-Fri type scenario
         start_day, end_day = days.split('-')
-        handle_day_list(end_day, end_hour, start_day, start_hour, time_frames)
+        weekly_hours(end_day, end_datetime, start_day, start_datetime, time_frames)
 
     array.append(time_frames)
 
 
-def handle_day_list(end_day, end_hour, start_day, start_hour, time_frames):
+def convert_string_time(hour_string):
+    start_format = '%I:%M %p' if ':' in hour_string else '%I %p'
+    return datetime.strptime(hour_string, start_format).time()
+
+
+def weekly_hours(end_day, end_hour, start_day, start_hour, time_frames):
     days_of_week = ['Mon', 'Tues', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
     start_day_index = days_of_week.index(start_day)
     end_day_index = days_of_week.index(end_day)
@@ -53,21 +58,21 @@ with open('public-data/restaurants.csv', newline='') as csvfile:
         name = row[0]
         time = row[1].split('/')
         multi_hours = []
-        if len(time) == 2:
-            for hour_range in time:
-                create_tuples(hour_range.strip(), multi_hours)
-
-        else:
-            create_tuples(time[0], multi_hours)
-
-        restaurant_data[name] = multi_hours
-    for name, val in restaurant_data.items():
-        print(name, val)
+        for hour_range in time:
+            create_schedules(hour_range.strip(), multi_hours)
+        # convert list of dict into single dict of weekday: (time, time)
+        hours_dict = multi_hours[0]
+        for hour in multi_hours:
+            hours_dict.update(hour)
+        restaurant_data[name] = hours_dict
 
 
 # API endpoint
-def is_open(restHours, target_datetime):
-    return True
+def is_open(rest_hours, target_datetime):
+    day_of_week = target_datetime.strftime('%a')
+    now_time = target_datetime.time()
+    start_time, end_time = rest_hours[day_of_week]
+    return start_time <= now_time <= end_time
 
 
 @app.route('/restaurants', methods=['GET'])
@@ -77,7 +82,7 @@ def get_open_restaurants():
         return jsonify({'error': 'You must provide a date time value'}), 400
 
     try:
-        target_datetime = datetime.strptime(datetime_str, '%m-%d %H:%M')
+        target_datetime = datetime.strptime(datetime_str, '%Y-%m-%d %H:%M')
     except ValueError:
         return jsonify({'error': 'Invalid datetime format. Use MM-DD HH:MM'}), 400
 
@@ -86,7 +91,11 @@ def get_open_restaurants():
         if is_open(restHours, target_datetime):
             open_restaurants.append(restaurant)
 
-    return jsonify({'open_restaurants': open_restaurants})
+    return jsonify({
+        'Day of the week': target_datetime.strftime('%a'),
+        'Requested hour': target_datetime.strftime('%I:%M %p'),
+        'open_restaurants': open_restaurants
+    })
 
 
 if __name__ == '__main__':
